7a9a6eefade517f96cb5e0b8fdb5c2f6f1c62e4d
Derek Webb <dwebb.af@gmail.com>
2016-02-27 16:32:28 -0800

    Edit page is visible

--- www/js/app.js
+++ www/js/app.js
@@ -5,33 +5,40 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('light-switch-mobile', ['ionic', 'light-switch-mobile.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 // Add application states
-.config(function($stateProvider, $urlRouterProvider) {
-   // Home
-   $stateProvider.state('home', {
-    url: '/home',
-    templateUrl: 'templates/home.html',
-    controller: 'HomeController'
-  });
-  
-  // Default route
-  $urlRouterProvider.otherwise('/home');
+.config(function ($stateProvider, $urlRouterProvider) {
+    // Home
+    $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    });
+
+    // Edit
+    $stateProvider.state('edit', {
+        url: '/edit',
+        templateUrl: 'templates/edit.html',
+        controller: 'EditController'
+    });
+
+    // Default route
+    $urlRouterProvider.otherwise('/home');
 });
---
+++ www/js/controllers/edit-controller.js
@@ -0,0 +1,42 @@
+angular.module('light-switch-mobile.controllers')
+
+.controller('EditController', function($scope, $rootScope, $http, $localStorage) {
+    $scope.lightSwitchList = null;
+    
+    $scope.init = function() {
+        $scope.getLightSwitchList();
+    };
+    
+    $scope.getLightSwitchList = function() {
+        $http.get($rootScope.lightSwitchUrl + "/api/light-switch?action=getLightSwitchList")
+        .then(function success(response) {
+            $scope.lightSwitchList = response.data;
+        }, function error(response) {
+            
+        });
+    };
+    
+    // $scope.lightSwitchClicked = function(lightSwitch) {
+    //     // Toggle the light switch
+    //     lightSwitch.active = !lightSwitch.active;
+    //     
+    //     // Stringify the light switch
+    //     var lightSwitchJsonString = angular.toJson(lightSwitch);
+    //     
+    //     // Update the server
+    //     $http({
+    //         url: $rootScope.lightSwitchUrl + "/api/light-switch?action=updateLightSwitch",
+    //         method: "GET",
+    //         params: {
+    //             lightSwitch: lightSwitchJsonString
+    //         }
+    //     })
+    //     .then(function success(response) {
+    //         var i = 0;
+    //     }, function error(response) {
+    //         var i = 0;
+    //     });
+    // };
+    
+    $scope.init();
+});
